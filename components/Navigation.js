d3a9339bb04a9db7b64ce3478d60579dd001e29a
Chrisdocs <chriswolfe.dev@gmail.com>
2022-09-15 10:41:57 -0600

    basic mobile nav menu setup

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,11 +1,30 @@
+import React from "react";
+import { useState } from "react";
+import styles from "../styles/Navigation.module.scss";
+
 export default function Navigation() {
+  const [navIsOpen, setNavIsOpen] = useState(false);
+
+	const handleToggle = () => {
+		setNavIsOpen(prev => !prev)
+	}
+
   return (
-    <div>
-      <div>Logo</div>
-			<div>Home</div>
-      <div>Search Bar</div>
-      <div>Login/out</div>
-      <div>Cart</div>
+    <div className={styles.navWrapper}>
+      <nav className={styles.navBar}>
+        <button className={styles.navToggle} onClick={handleToggle}>{navIsOpen ? "Close" : "Open"}</button>
+        <ul className={navIsOpen ? styles.showMenu : styles.menuNav}>
+					<li>
+						<a href="/">Home</a>
+					</li>
+					<li>
+						<a href="/products">Products</a>
+					</li>
+					<li>
+						<a href="/cart">Cart</a>
+					</li>
+				</ul>
+      </nav>
     </div>
   );
 }
